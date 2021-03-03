55dc7fb2c3bafd2154bc15e944b2936c33a45253
Guilherme C. Souza <gcsgacc@gmail.com>
2021-03-03 19:23:06 -0300

    Add group functionality

--- test/spec.js
+++ test/spec.js
@@ -18,8 +18,8 @@ beforeEach(async function(){
 });
 
 afterEach(async function(){
-    await mongo.account.deleteOne({ name: 'test-a' });
-    await mongo.account.deleteOne({ name: 'test-z' });
+    await mongo.account.deleteOne();
+    await mongo.group.deleteOne();
     await okaeri.stop();
 });
 
@@ -154,6 +154,141 @@ describe('Okaeri', function(){
             let a1 = await res.next();
             assert.strictEqual(a1.name, 'test-z');
             assert(!await res.next());
+        });
+
+    });
+
+    describe('Creating a Group', function(){
+
+        it('Should fail when input is invalid', async function(){
+            let res = await okaeri.createGroup();
+            assert(!res.ok);
+        });
+
+        it('Should create a group in database', async function(){
+            let res = await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            assert(res.ok);
+            assert(await mongo.group.findOne({ name: 'g1' }));
+        });
+
+        it('Should fail when the group already exists', async function(){
+            await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            let res = await okaeri.createGroup({ name: 'g2', code: 'g1' });
+            assert(!res.ok);
+            assert.strictEqual(res.type, 'conflict');
+        });
+
+    });
+
+    describe('Updating a Group', function(){
+
+        it('Should fail when input is invalid', async function(){
+            let res = await okaeri.updateGroup('abdcfe61524635124', false);
+            assert(!res.ok);
+        });
+
+        it('Should fail when group doesn\'t exist', async function(){
+            let res = await okaeri.updateGroup('abdcfe61524635124');
+            assert(!res.ok);
+            assert.strictEqual(res.type, 'unknown');
+        });
+
+        it('Should update group in database', async function(){
+            let { id } = await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            let res = await okaeri.updateGroup(id, { name: 'g0' });
+            assert(res.ok);
+            assert(await mongo.group.findOne({ name: 'g0' }));
+        });
+
+    });
+
+    describe('Fetching Group info', function(){
+
+        it('Should fail when group doesn\'t exist', async function(){
+            let res = await okaeri.readGroup('abdcfe61524635124');
+            assert(!res.ok);
+            assert.strictEqual(res.type, 'unknown');
+        });
+
+        it('Should return group info', async function(){
+            let { id } =  await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            let res = await okaeri.readGroup(id);
+            assert(res.ok);
+            assert.strictEqual(res.group.name, 'g1');
+        });
+
+    });
+
+    describe('Removing group', function(){
+
+        it('Should fail when group doesn\'t exist', async function(){
+            let res = await okaeri.removeGroup('abdcfe61524635124');
+            assert(!res.ok);
+            assert.strictEqual(res.type, 'unknown');
+        });
+
+        it('Should remove the group from db', async function(){
+            let { id } = await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            let res = await okaeri.removeGroup(id);
+            assert(res.ok);
+            assert.strictEqual(res.code, 'g1');
+            let r = await okaeri.readGroup(id);
+            assert(!r.ok);
+        });
+
+    });
+
+    describe('Query groups', function(){
+
+        it('Should return all group records', async function(){
+            await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            await okaeri.createGroup({ name: 'g2', code: 'g2' });
+            let res = await okaeri.queryGroups();
+            assert.strictEqual(res.groups.length, 2);
+        });
+
+        it('Should filter returned groups', async function(){
+            await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            await okaeri.createGroup({ name: 'g2', code: 'g2' });
+            let res = await okaeri.queryGroups({ name: { $regex: /2/ } });
+            assert.strictEqual(res.groups.length, 1);
+            assert.strictEqual(res.groups[0].name, 'g2');
+        });
+
+    });
+
+    describe('Manage group accounts', function(){
+
+        it('Should fail when account doesn\'t exist', async function(){
+            let r1 = await okaeri.addAccountToGroup('234235abfd5765fe', '234235abfd5765fe');
+            assert(!r1.ok);
+
+            let r2 = await okaeri.removeAccountFromGroup('234235abfd5765fe', '234235abfd5765fe');
+            assert(!r2.ok);
+        });
+
+        it('Should fail when group doesn\'t exist', async function(){
+            let { id } = await okaeri.createAccount({ name: 'test-a', password: 'foobarbaz' });
+
+            let r1 = await okaeri.addAccountToGroup(id, '234235abfd5765fe');
+            assert(!r1.ok);
+
+            let r2 = await okaeri.removeAccountFromGroup(id, '234235abfd5765fe');
+            assert(!r2.ok);
+        });
+
+        it('Should add account to group', async function(){
+            let { id } = await okaeri.createAccount({ name: 'test-a', password: 'foobarbaz' });
+            let { id: gid } = await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            let res = await okaeri.addAccountToGroup(id, gid);
+            assert(res.ok);
+        });
+
+        it('Should remove account to group', async function(){
+            let { id } = await okaeri.createAccount({ name: 'test-a', password: 'foobarbaz' });
+            let { id: gid } = await okaeri.createGroup({ name: 'g1', code: 'g1' });
+            let res = await okaeri.removeAccountFromGroup(id, gid);
+            assert(res.ok);
         });
 
     });
